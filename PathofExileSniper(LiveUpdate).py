41b34416a4ea62326ab42b7d22f79ca66bbb45a8
Knathmer <103916780+Knathmer@users.noreply.github.com>
2023-01-13 19:07:27 -0600

    Update PathofExileSniper(LiveUpdate).py
    Removed instructions from script to Readme file.

--- PathofExileSniper(LiveUpdate).py
+++ PathofExileSniper(LiveUpdate).py
@@ -1,21 +1,3 @@
-#------------Prerequisites------------#
-# To use this script, you must have/do the following:
-# 1. All of the modules installed.
-# 2. In the initialization section, you must change the path to your chrome profile. Simply replace the name of the profile with your profile name.
-# 3. In the initialization section, you must change the path to your chromedriver.exe. Simply replace the path with the path to your chromedriver.exe. I recommend putting it in the same folder as this script.
-# 4. Set your threshold prices for chaos, divine, and mirror orbs.
-# 5. Chrome must be closed before running the script.
-# 6. Before closing out of chrome, you must copy the link to the live update page. This can be done by clicking the "Live Update" button on the trade site. Keep it on your clipboard.
-# 7. You must have a default profile in chrome. This is the profile that will be used to run the script.
-# 8. Path of Exile must be running.
-
-#------------Use------------#
-# To use this script, run it. Once it starts you will see a Chrome page to select your default profile. You want to select the profile that contains your Path of Exile login information.
-# If you are unable to select your profile, you may need to change the ENTIRE path to your --profile-directory in the initialization section. Locate the folder of your default profile and copy the ENTIRE path.
-# Once you have selected your profile, click on the URL bar and paste the link to the live update page. Then press enter.
-# The script will now run. You can minimize the chrome window and it will continue to run in the background. If you want to stop the script, simply close the chrome window.
-# If you want to change the threshold prices, you can do so in the initialization section. You will have to restart the script for the changes to take effect.
-
 #------------Initialization------------#
 
 from selenium import webdriver
