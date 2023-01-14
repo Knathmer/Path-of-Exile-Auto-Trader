de947f9fd021fede9bf6490fe17afe309775c4ad
Knathmer <103916780+Knathmer@users.noreply.github.com>
2023-01-13 18:55:24 -0600

    Add files via upload

--- PathofExileSniper(LiveUpdate).py
+++ PathofExileSniper(LiveUpdate).py
@@ -1,4 +1,23 @@
+#------------Prerequisites------------#
+# To use this script, you must have/do the following:
+# 1. All of the modules installed.
+# 2. In the initialization section, you must change the path to your chrome profile. Simply replace the name of the profile with your profile name.
+# 3. In the initialization section, you must change the path to your chromedriver.exe. Simply replace the path with the path to your chromedriver.exe. I recommend putting it in the same folder as this script.
+# 4. Set your threshold prices for chaos, divine, and mirror orbs.
+# 5. Chrome must be closed before running the script.
+# 6. Before closing out of chrome, you must copy the link to the live update page. This can be done by clicking the "Live Update" button on the trade site. Keep it on your clipboard.
+# 7. You must have a default profile in chrome. This is the profile that will be used to run the script.
+# 8. Path of Exile must be running.
+
+#------------Use------------#
+# To use this script, run it. Once it starts you will see a Chrome page to select your default profile. You want to select the profile that contains your Path of Exile login information.
+# If you are unable to select your profile, you may need to change the ENTIRE path to your --profile-directory in the initialization section. Locate the folder of your default profile and copy the ENTIRE path.
+# Once you have selected your profile, click on the URL bar and paste the link to the live update page. Then press enter.
+# The script will now run. You can minimize the chrome window and it will continue to run in the background. If you want to stop the script, simply close the chrome window.
+# If you want to change the threshold prices, you can do so in the initialization section. You will have to restart the script for the changes to take effect.
+
 #------------Initialization------------#
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -14,16 +33,17 @@ length = 0
 win = gw.getWindowsWithTitle('Path of Exile')[0] # Sets win = to the path of exile window
 
 options = webdriver.ChromeOptions()
-options.add_argument(r"--user-data-dir=C:\Users\Eraj\AppData\Local\Google\Chrome\User Data")
-options.add_argument(r'--profile-directory=C:\Users\Eraj\AppData\Local\Google\Chrome\User Data\Profile 2')
-driver = webdriver.Chrome(executable_path=r'C:/Program Files (x86)\chromedriver.exe', chrome_options=options)
+options.add_argument(r"--user-data-dir=C:\Users\(replace this with your username)\AppData\Local\Google\Chrome\User Data") #Path to your Chrome profile
+options.add_argument(r'--profile-directory=C:\Users\(replace this with your username)\AppData\Local\Google\Chrome\User Data\Profile 2') #Keep Profile 2 as your default profile
+driver = webdriver.Chrome(executable_path=r'C:/Program Files (x86)\chromedriver.exe', chrome_options=options) #Path to your chromedriver.exe
 
-url = 'https://www.google.co.in'
+url = 'https://www.google.co.in' # This needs to be set to Google so that the script can import your default profile to bypass Cloudflare bot protection.
 
 #------------Conditions------------#
-chaoslimit = 10 # Set the max cost for chaos
-exaltlimit = 100 # Set the max cost for exalts
-mirrorlimit = 1.5 # Set the max cost for mirrors
+
+chaoslimit = 10 # Set the price threshold for chaos orbs
+divinelimit = 100 # Set the price threshold divine orbs
+mirrorlimit = 1.5 # Set the price threshold for mirrors
 
 #------------Functions------------#
 
@@ -53,11 +73,12 @@ def send():
     message()
 
 #------------Main Program------------#
+
 driver.get(url)
 
 time.sleep(10)
 
-WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, 'results')))
+WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, 'results'))) # Waits for the results to load
 
 while loop == True:
     
@@ -70,9 +91,9 @@ while loop == True:
     
     for item in items:
         length += 1
+        print("test")
         
         print('----------------------------------------------------------------')
-        
         
         try: # Tries to find the location of price in the main box
         
@@ -85,10 +106,10 @@ while loop == True:
                     print(str(amount)+' CHAOS \n'+listdate.text+'\n') # String formatting of the cost and date listed
                     send()
                     
-            if 'exalted' in price.text:
+            if 'divine' in price.text:
                 amount = float(price.text.split(' ')[1])
-                if amount < exaltlimit:
-                    print(str(amount)+' EXALT(s) \n'+listdate.text+'\n')
+                if amount < divinelimit:
+                    print(str(amount)+' DIVINE(s) \n'+listdate.text+'\n')
                     send()
                     
             if 'mirror' in price.text:
@@ -109,10 +130,10 @@ while loop == True:
                     print(str(amount)+' CHAOS \n'+listdate.text+'\n')
                     send()
                         
-            if 'Exalted Orb' in price.text:
-                amount = float(price.text.split('\n')[1].split('×Exalted Orb')[0])
-                if amount < exaltlimit:
-                    print(str(amount)+' EXALT(s) \n'+listdate.text+'\n')
+            if 'Divine Orb' in price.text:
+                amount = float(price.text.split('\n')[1].split('×Divine Orb')[0])
+                if amount < divinelimit:
+                    print(str(amount)+' DIVINES(s) \n'+listdate.text+'\n')
                     send()
                     
             if 'Mirror of Kalandra' in price.text:
@@ -122,4 +143,3 @@ while loop == True:
                     send()
                     
                     
-
